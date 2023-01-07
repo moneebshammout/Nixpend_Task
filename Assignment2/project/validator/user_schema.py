bd13109193038445ede4049992087d839de1d162
moneeb shammout <moneeb41@gmail.com>
2023-01-08 00:24:40 +0200

    test push

--- Assignment2/project/validator/user_schema.py
+++ Assignment2/project/validator/user_schema.py
@@ -5,3 +5,5 @@ class getPDFSchema(Schema):
     fullName = fields.Str(required=True, validate=validate.Length(min=8))
     email = fields.Email(required=True)
     phone = fields.Int(required=True, validate=validate.Range(min=10))
+
+#//test
